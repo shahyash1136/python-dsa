0f3d1d78161a8e8109eb7457f72a44030861c43d
Yash Shah <43175218+shahyash1136@users.noreply.github.com>
2026-08-26 23:28:43 +0530

    Add doubly linked list pop/prepend implementation
    Introduce DoubleLinkedList/DLL-Pop.py which defines a Node and DoubleLinkedList. Implements pop() to remove and return the last node (raises IndexError when empty) and prepend(val) to insert at the head. Correctly handles single-node and multi-node cases and properly updates prev/next links.

---
+++ DoubleLinkedList/DLL-Pop.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def pop(self):
+        if self.head is None:
+            raise IndexError("Index out of bound")
+        elif self.head.next is None:
+            temp = self.head
+            self.head = None
+            return temp
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+
+            temp.prev.next = None
+            temp.prev = None
+            return temp
+
+    def prepend(self,val):
+        new_node = Node(val)
+        new_node.next = self.head
+        if self.head is not None:
+            self.head.prev = new_node
+        self.head = new_node
